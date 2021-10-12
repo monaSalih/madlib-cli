297fbc9a60d8c80796446be7b080d2d4b04e6c2c
monaSalih <monaalobisat@gmail.com>
2021-10-12 21:53:36 +0300

    read File

---
+++ madlib_cli/madlib.py
@@ -0,0 +1,11 @@
+# import re
+
+def read_template(filePath):
+    openFile=open(filePath)
+    # readFile=openFile.read
+    return openFile.read()
+
+def parse_template(srtingTemplet ):
+    stringTemplet.split(" ")
+    
+ 
