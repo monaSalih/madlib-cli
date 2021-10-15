937e3bf83fcf363d82af1317a495dee085060864
monaSalih <monaalobisat@gmail.com>
2021-10-15 23:56:03 +0300

    input game

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,6 +1,6 @@
 import re
 
-print ("Welcom to our madlib game i hope you enjoying")
+print ("Welcom to our madlib game i hope you enjoying, please answer the question right to built your story")
 def read_template(filePath):
     openFile=open(filePath)
     # readFile=openFile.read
@@ -8,13 +8,12 @@ def read_template(filePath):
 
 def parse_template(upDateText):
     """
-    in this function we split new sentene and add it to list after that remove adjective from inter sentence 
+    in this function we split new sentene and add it to list fter that remove adjective from inter sentence 
     by using regeicx ,and after that commpined into new sentence 
     """
     parseEmp=''
     newTestList=[]
     addTextToList=upDateText.split(' ')
-    print(addTextToList)
     AdjectiveReg=r"^{\w+}|\.$"
     for i in addTextToList:
         if re.match(AdjectiveReg,i)==None :
@@ -35,3 +34,24 @@ def merge(upDateText,indexWord):
     replace {} inside upDateText with index from indexWord by order 
     """
     return upDateText.format(*indexWord)
+
+def output_file(result):
+    with open("assets/game_output.txt", "w") as f:
+        f.write(result)
+
+     
+def game_data():
+    game_text = read_template("assets/make_me_a_video_game_template.txt")
+    new_text, text_saved = parse_template(game_text)
+    game_input = []
+
+    for i in range(len(text_saved)):
+        x = input('enter a {} > '.format(text_saved[i]))
+        game_input.append(x)
+    result = new_text.format(*game_input)
+    print(result)
+    output_file(result)
+
+    
+
+# get_data()
